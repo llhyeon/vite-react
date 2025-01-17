f8e449d0759989c4bfc3f704f6738a388afb6e9f
llhyeon <0617mosses@gmail.com>
2025-01-17 17:59:54 +0900

    [구성] 기본 환경구성

---
+++ src/app/app.tsx
@@ -0,0 +1,7 @@
+import './app.css';
+
+function App() {
+  return <div className="app">hello</div>;
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
 
 const viteConfig = defineConfig({
-  server: {
-    host: "localhost",
-    port: 3000,
-  },
-  preview: {
-    host: "localhost",
-    port: 8080,
-  },
-  css: {
-    // 파일 소스맵 명시
-    devSourcemap: true,
-  },
+    plugins: [
+        react({
+            jsxRuntime: 'automatic',
+        }),
+    ],
+    server: {
+        host: 'localhost',
+        port: 3000,
+    },
+    preview: {
+        host: 'localhost',
+        port: 8080,
+    },
+    css: {
+        // 파일 소스맵 명시
+        devSourcemap: true,
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
 });
 
 export default viteConfig;
