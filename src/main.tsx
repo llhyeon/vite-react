f60a163f57d806de24afec93c0b12b3bcbf6c462
llhyeon <0617mosses@gmail.com>
2025-01-18 18:33:55 +0900

    [수정] main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app/app";
-import "./styles/main.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/app';
+import './styles/main.css';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 if (root) {
   createRoot(root).render(
